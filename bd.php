c9a0a3de8a1b600e779302b6d7bfcf07bf0f299f
Brayan Patiño <bpatinoc@choucairtesting.com>
2023-04-28 18:18:44 -0500

    se corrige error en la conexion con bd

--- bd.php
+++ bd.php
@@ -3,7 +3,7 @@
 $servidor="localhost"; //127.0.0.1
 $baseDeDatos="u758506060_inventario";
 $usuario="u758506060_brayann91";
-$contrasena="2P$xmJqMOc+m";
+$contrasena="3:7LL^rJ";
 try {
     $conexion= new PDO("mysql:host=$servidor;dbname=$baseDeDatos", $usuario, $contrasena);
 } catch (Exception $ex) {

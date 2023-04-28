9a7d7e2a1be29957294d124f2bf41f935dca403d
Brayan Patiño <bpatinoc@choucairtesting.com>
2023-04-28 18:03:04 -0500

    se cambian los datos de la bd

--- bd.php
+++ bd.php
@@ -1,9 +1,9 @@
 <?php
 
 $servidor="localhost"; //127.0.0.1
-$baseDeDatos="inventario";
-$usuario="root";
-$contrasena="";
+$baseDeDatos="u758506060_inventario";
+$usuario="u758506060_brayann91";
+$contrasena='@3W27iZ[D$i';
 try {
     $conexion= new PDO("mysql:host=$servidor;dbname=$baseDeDatos", $usuario, $contrasena);
 } catch (Exception $ex) {

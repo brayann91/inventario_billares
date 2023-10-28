0d808426a78dabc6976f21d4e42f36b597403352
Brayan Patiño <bpatinoc@choucairtesting.com>
2023-10-28 12:16:03 -0500

    se cambia la contraseña de la base de datos

--- bd.php
+++ bd.php
@@ -3,7 +3,7 @@
 $servidor="127.0.0.1:3306"; //127.0.0.1
 $baseDeDatos="u758506060_inventario";
 $usuario="u758506060_brayann91";
-$contrasena="3:7LL^rJ";
+$contrasena="!9gQb9nYBPG";
 $opciones = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET time_zone = '-05:00'");
 try {
     $conexion = new PDO("mysql:host=$servidor;dbname=$baseDeDatos", $usuario, $contrasena, $opciones);
